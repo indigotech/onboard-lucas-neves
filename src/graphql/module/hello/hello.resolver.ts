b6f237643a28cba3eb44623dc5208b62d49a3d41
Lucas Neves <lucasfneves14@gmail.com>
2021-09-12 17:00:20 -0300

    mutation setup

--- src/graphql/module/hello/hello.resolver.ts
+++ src/graphql/module/hello/hello.resolver.ts
@@ -1,8 +1,16 @@
+import { User } from "../../../db/entity";
+
 const resolvers = {
   Query: {
     hello(): string {
       return "Hello, world!";
     },
   },
+  Mutation: {
+    createUser(_, { data }): User {
+      return { id: 1, ...data };
+    },
+  },
 };
+
 export default resolvers;
--- src/graphql/module/hello/hello.type.ts
+++ src/graphql/module/hello/hello.type.ts
@@ -7,6 +7,24 @@ const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  input UserInput {
+    name: String
+    email: String
+    password: String
+    birthDate: String
+  }
+
+  type User {
+    id: ID!
+    name: String
+    email: String
+    birthDate: String
+  }
+
+  type Mutation {
+    createUser(data: UserInput): User
+  }
 `;
 
 export default typeDefs;

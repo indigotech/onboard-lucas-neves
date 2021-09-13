742b69ab48b60c5ca4f6bb584e3b0c4985e16f0c
Taqtile <taqtile@Kona.local>
2021-09-13 17:43:03 -0300

    fix: password validation error

--- src/domain/password-validation.ts
+++ src/domain/password-validation.ts
@@ -1,3 +1,6 @@
 export function passwordValidation(value: string): boolean {
-  return value.length < 7 || (/[a-zA-Z]/g.test(value) && /\d/g.test(value));
+  console.log(value.length < 7);
+  console.log(/[a-zA-Z]/g.test(value));
+  console.log(/\d/g.test(value));
+  return value.length < 7 || !(/[a-zA-Z]/g.test(value) && /\d/g.test(value));
 }

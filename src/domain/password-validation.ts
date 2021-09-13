889d752fbb4bc5001f4ebe8d570929c211dde770
Taqtile <taqtile@Kona.local>
2021-09-13 18:19:42 -0300

    console logs removal

--- src/domain/password-validation.ts
+++ src/domain/password-validation.ts
@@ -1,6 +1,3 @@
 export function passwordValidation(value: string): boolean {
-  console.log(value.length < 7);
-  console.log(/[a-zA-Z]/g.test(value));
-  console.log(/\d/g.test(value));
   return value.length < 7 || !(/[a-zA-Z]/g.test(value) && /\d/g.test(value));
 }

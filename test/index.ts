1847f4beeaa05ea86b68c3247e7bafec90d4b38e
Taqtile <taqtile@Kona.local>
2021-09-14 16:33:11 -0300

    dotenv setup

--- test/index.ts
+++ test/index.ts
@@ -3,14 +3,17 @@ import { setupServer } from "../src/graphql/graphql-server";
 import { setupDb } from "@db/config";
 import { expect } from "chai";
 import * as request from "supertest";
-//const request = require("supertest");
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 describe("hello test", function () {
-  before(async () => {
+  it("test one: setup server and database", async function () {
     await setupDb();
     await setupServer();
   });
-  it("hello query", async function () {
+
+  it("test two: hello query", async function () {
     request("localhost:4000")
       .post("/graphql")
       .send({

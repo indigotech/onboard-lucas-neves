cfde122e3fd7916a93ac5ca7de4c7b6920135deb
Taqtile <taqtile@Kona.local>
2021-09-14 15:13:08 -0300

    hello query test

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,25 @@
+import "module-alias/register";
+import { setupServer } from "../src/graphql/graphql-server";
+import { setupDb } from "@db/config";
+
+const request = require("supertest");
+
 describe("hello test", function () {
-  it("says teste", function () {
-    console.log("teste");
+  before(async () => {
+    await setupDb();
+    await setupServer();
+  });
+  it("hello query", async function () {
+    request("localhost:4000")
+      .post("/graphql")
+      .send({
+        query: "{ hello }",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        console.log(res.text);
+      });
   });
 });

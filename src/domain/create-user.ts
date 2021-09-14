ddcba9950ab6de2297df2d414ca786bcbcd9c871
Taqtile <taqtile@Kona.local>
2021-09-14 10:39:51 -0300

    fix: user params create order

--- src/domain/create-user.ts
+++ src/domain/create-user.ts
@@ -3,7 +3,7 @@ import * as bcrypt from "bcrypt";
 
 export async function createUser(data: User): Promise<User> {
   const password = await bcrypt.hash(data.password, 12);
-  const newUser = User.create({ password, ...data });
+  const newUser = User.create({ ...data, password });
   await newUser.save();
   console.log("New User has been saved. Id: ", newUser.id);
   return newUser;

425c1ca0264ccaaf343e0c1fdf907138386dfe0c
Taqtile <taqtile@Kona.local>
2021-09-15 19:52:41 -0300

    fix: test for db too

--- test/index.ts
+++ test/index.ts
@@ -4,6 +4,7 @@ import { setupDb } from "@db/config";
 import { expect } from "chai";
 import * as request from "supertest";
 import * as dotenv from "dotenv";
+import * as bcrypt from "bcrypt";
 import { User } from "@db/entity";
 
 describe("hello test", function () {
@@ -32,26 +33,28 @@ describe("hello test", function () {
       .post("/graphql")
       .send({
         query:
-          'mutation { createUser(data: {name: "Nome", birthDate: "birth", email: "teste@teste1323f23332333.com", password: "adasdgh5jak"}){id name email birthDate} }',
+          'mutation { createUser(data: {name: "Nome", birthDate: "birth", email: "teste@teste.com", password: "senha123"}){id name email birthDate} }',
       })
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(200)
-      .then((res) => {
+      .then(async (res) => {
         expect(res.body.data.createUser).to.have.property("id");
         expect(res.body.data.createUser).to.have.property("name");
         expect(res.body.data.createUser).to.have.property("email");
         expect(res.body.data.createUser).to.have.property("birthDate");
+        expect(res.body.data.createUser.name).to.be.eq("Nome");
+        expect(res.body.data.createUser.email).to.be.eq("teste@teste.com");
+        expect(res.body.data.createUser.birthDate).to.be.eq("birth");
+
+        const user = await User.findOne();
+        expect(user.name).to.be.eq("Nome");
+        expect(user.email).to.be.eq("teste@teste.com");
+        expect(user.birthDate).to.be.eq("birth");
+        expect(await bcrypt.compare("senha123", user.password)).to.be.true;
       });
   });
-  it("test three: check with user was created", async function () {
-    const user = await User.findOne();
-    expect(user).to.have.property("id");
-    expect(user).to.have.property("name");
-    expect(user).to.have.property("email");
-    expect(user).to.have.property("birthDate");
-  });
-  after(function () {
+  afterEach(function () {
     User.delete({});
   });
 });

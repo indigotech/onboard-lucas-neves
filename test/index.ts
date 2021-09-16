b9939fb614b478c83646663f18b3104b4edd1afc
Taqtile <taqtile@Kona.local>
2021-09-16 10:38:58 -0300

    fix: async/await at supertest response

--- test/index.ts
+++ test/index.ts
@@ -29,7 +29,7 @@ describe("hello test", function () {
   });
 
   it("test two: mutation createUser", async function () {
-    await request("localhost:4000")
+    const res = await request("localhost:4000")
       .post("/graphql")
       .send({
         query:
@@ -37,24 +37,21 @@ describe("hello test", function () {
       })
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      .expect(200)
-      .then(async (res) => {
-        expect(await res.body.data.createUser).to.have.property("id");
-        expect(await res.body.data.createUser).to.have.property("name");
-        expect(await res.body.data.createUser).to.have.property("email");
-        expect(await res.body.data.createUser).to.have.property("birthDate");
-        expect(await res.body.data.createUser.name).to.be.eq("Nome");
-        expect(await res.body.data.createUser.email).to.be.eq(
-          "teste@teste.com"
-        );
-        expect(await res.body.data.createUser.birthDate).to.be.eq("birth");
+      .expect(200);
 
-        const user = await User.findOne();
-        expect(user.name).to.be.eq("Nome");
-        expect(user.email).to.be.eq("teste@teste.com");
-        expect(user.birthDate).to.be.eq("birth");
-        expect(await bcrypt.compare("senha123", user.password)).to.be.true;
-      });
+    expect(res.body.data.createUser).to.have.property("id");
+    expect(res.body.data.createUser).to.have.property("name");
+    expect(res.body.data.createUser).to.have.property("email");
+    expect(res.body.data.createUser).to.have.property("birthDate");
+    expect(res.body.data.createUser.name).to.be.eq("Nome");
+    expect(res.body.data.createUser.email).to.be.eq("teste@teste.com");
+    expect(res.body.data.createUser.birthDate).to.be.eq("birth");
+
+    const user = await User.findOne();
+    expect(user.name).to.be.eq("Nome");
+    expect(user.email).to.be.eq("teste@teste.com");
+    expect(user.birthDate).to.be.eq("birth");
+    expect(await bcrypt.compare("senha123", user.password)).to.be.true;
   });
   afterEach(async function () {
     await User.delete({});
